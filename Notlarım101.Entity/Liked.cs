eb6e2fb58a3d7e71702a361a5474185bb12cd72c
alparslaneroglu <furkanalparslaneroglu@gmail.com>
2021-12-13 14:32:38 +0300

    Katmanlı Mimari

---
+++ Notlarım101.Entity/Liked.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notlarım101.Entity
+{
+    [Table("tblLiked")]
+    public class Liked
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public virtual Note Note { get; set; } //Notlar like edilir.
+        public virtual NotlarimUser LikedUser { get; set; }
+
+    }
+}
---
+++ Notlarım101.Entity/NotlarimUser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notlarım101.Entity
+{
+    [Table("tblNotlarimUsers")]
+    public class NotlarimUser : MyEntityBase
+    {
+       [StringLength(30)]
+        public string Name { get; set; }
+        [StringLength(30)]
+        public string Surname { get; set; }
+        [StringLength(30),Required]
+        public string Username { get; set; }
+        [StringLength(100),Required]
+        public string Email { get; set; }
+        [StringLength(100),Required]
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        [Required]
+        public Guid ActivateGuid { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public List<Note> Notes { get; set; }
+        public List<Comment> Comments { get; set; }
+        public List<Liked> Likes { get; set; }
+
+
+    }
+}
